2f2f9923ebcba7e98405f344cd99420d3a6fd2dc
Willy Noubissie <willyjunior91@gmail.com>
2022-06-09 19:46:10 +0200

    remove DockerFile

---
+++ src/resources/bookings/mocks/all-bookings.mock.ts
@@ -0,0 +1,45 @@
+export default [
+  {
+    id: '4d1c11d6-2436-4742-838d-3d1d1063a177',
+    updatedAt: '2021-08-06T03:29:25.957Z',
+    createdAt: '2021-08-06T03:29:25.957Z',
+    startTime: '08:00:00',
+    endTime: '09:00:00',
+    description: 'is Awesome',
+    title: 'Strapi',
+    room: {
+      id: '0ebd73ca-0ca6-4a65-9a35-97441bfc7200',
+      updatedAt: '2021-08-02T22:31:23.219Z',
+      createdAt: '2021-08-02T22:31:23.219Z',
+      name: 'sensor',
+      description: 'Et molestiae possimus facilis natus quia.',
+      compagny: {
+        id: 2,
+        name: 'Pepsi',
+        description: 'Quas qui est explicabo omnis.',
+      },
+    },
+  },
+  {
+    id: '7327a4f6-fe4a-41ef-b897-c4b1371f4887',
+    updatedAt: '2021-08-06T03:35:16.000Z',
+    createdAt: '2021-08-06T03:35:01.057Z',
+    startTime: '16:00:00',
+    endTime: '17:00:00',
+    description: 'Library',
+    title: 'React',
+    room: {
+      id: '6250be1a-06f3-4b99-9ad9-c41db98607a4',
+      updatedAt: '2021-08-02T22:31:23.169Z',
+      createdAt: '2021-08-02T22:31:23.169Z',
+      name: 'web-readiness',
+      description: 'Dolorem ea adipisci eius sed.',
+      compagny: {
+        id: 1,
+        name: 'Coke',
+        description:
+          'Culpa est consequatur ea veritatis molestias est tenetur deserunt quidem.',
+      },
+    },
+  },
+];
